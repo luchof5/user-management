a6d42680298c476ba25ee90f35446e5bd2110af8
LuchoF5 <you@example.com>
2024-09-12 23:45:13 -0300

    add getUser

--- models.js
+++ models.js
@@ -1,25 +1,49 @@
-import { existsSync, readFileSync, writeFileSync } from "node:fs";
-import { randomUUID } from "node:crypto";
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { randomUUID, createHash } from 'node:crypto'
+import { handleError } from './utils/handleError.js'
 // Averiguar que importar de NODE para realizar el hash del pass
+
 // Averiguar como "activar" la lectura de las variables de entorno del archivo .env (dotenv)
-import { handleError } from "./utils/handleError.js";
+import dotenv from 'dotenv'
 
 // 1° recuperar variables de entorno
+dotenv.config()
+const PATH_FILE_USER = process.env.PATH_FILE_USER
+const PATH_FILE_ERROR = process.env.PATH_FILE_ERROR
 
 // 2° Declarar los metodos
 
-const getUsers = () => {
+const getUsers = (urlFile) => {
   try {
+    if (!urlFile) {
+      throw new Error('Acces deneid')
+    }
+
+    const exists = existsSync(urlFile)
+
+    if (!exists) {
+      writeFileSync(urlFile, JSON.stringify([]))
+
+      return []
+    }
+
+    const users = JSON.parse(readFileSync(urlFile))
+
+    return users
   } catch (error) {
-    // const objError = handleError()
-    // return objError;
+    const objError = handleError(error, PATH_FILE_ERROR)
+
+    return objError
   }
-};
+}
+
+// const prueba = getUsers(PATH_FILE_USER)
+// console.log(prueba)
 
 const getUserById = (id) => {
   try {
   } catch (error) {}
-};
+}
 
 // addUser recibe un objeto con toda la data para el nuevo usuario
 // valida que esten los datos míminos para añadir un nuevo usuario
@@ -30,7 +54,7 @@ const getUserById = (id) => {
 const addUser = (userData) => {
   try {
   } catch (error) {}
-};
+}
 
 // todos los datos del usuario seleccionado se podrían modificar menos el ID
 // si se modifica la pass debería ser nuevamente hasheada
@@ -38,11 +62,11 @@ const addUser = (userData) => {
 const updateUser = (userData) => {
   try {
   } catch (error) {}
-};
+}
 
 const deleteUser = (id) => {
   try {
   } catch (error) {}
-};
+}
 
-export { getUsers, getUserById, addUser, updateUser, deleteUser };
+export { getUsers, getUserById, addUser, updateUser, deleteUser }
